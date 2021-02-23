dc26e5fd06830e97a72eaf039555bf8a4b83f027
tjf91 <tudorjflorea@gmail.com>
2021-02-22 19:26:20 -0700

    initial commit, made folder structure

--- server.js
+++ server.js
@@ -2,6 +2,15 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 require('dotenv').config()
+const {SERVER_PORT, MONGO_URI}= process.env
+const mongoose = require('mongoose');
+
+mongoose.connect(MONGO_URI,{useNewUrlParser: true, useUnifiedTopology: true})
+const db=mongoose.connection
+db.on('error', console.error.bind(console, 'connection error'))
+db.once('open', ()=> console.log('Mongo DB connected'))
+
+
 
 app.use(cors())
 app.use(express.static('public'))
@@ -13,6 +22,6 @@ app.get('/', (req, res) => {
 
 
 
-const listener = app.listen(process.env.PORT || 3000, () => {
-  console.log('Your app is listening on port ' + listener.address().port)
+const listener = app.listen(SERVER_PORT, () => {
+  console.log('Your app is listening on port ' + SERVER_PORT)
 })
